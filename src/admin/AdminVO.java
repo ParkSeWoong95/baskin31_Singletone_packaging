21516ddcc8206110b6051cf3279c39e6d3a690bd
RIRINTO <54238243+RIRINTO@users.noreply.github.com>
2021-01-27 10:00:16 +0900

    .

---
+++ src/admin/AdminVO.java
@@ -0,0 +1,12 @@
+package admin;
+
+public class AdminVO {
+	private String id = "admin";
+	private String pw = "p@ssW0rd";
+	public String getId() {
+		return id;
+	}
+	public String getPw() {
+		return pw;
+	}
+}
