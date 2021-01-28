eb3982c64cee407f29da2334e883c0bd42511bd6
RIRINTO <ririnto@kakao.com>
2021-01-29 01:15:28 +0900

    Update Poi.java

--- src/poi/Poi.java
+++ src/poi/Poi.java
@@ -197,8 +197,6 @@ public class Poi {
 		return orderDetailsList;
 	}
 	
-<<<<<<< HEAD
-<<<<<<< HEAD
 	public static boolean writeExcelFile() {
 		try {
 			FileOutputStream file = new FileOutputStream("db\\db2.xlsx");
@@ -208,10 +206,4 @@ public class Poi {
 		}
 		return false;
 	}
-=======
-	
->>>>>>> parent of 1bd3684... .
-=======
-	
->>>>>>> parent of 1bd3684... .
 }
